29a1a193de22da43977c4c3250029589c1bf4199
Qhama Nongogo <s225694492@outlook.com>
2025-09-11 03:07:46 +0200

    I added table for list of users in admin

---
+++ FridgeManagementSystem/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FridgeManagementSystem.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+
+
+
+    }
+}
