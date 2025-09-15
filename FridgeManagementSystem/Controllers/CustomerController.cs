e88895c094a44b5153a28a321772ab0c05d6899f
Qhama Nongogo <s225694492@outlook.com>
2025-09-16 00:00:41 +0200

    I have added customer table in the admin that retrieves customers from the database

--- FridgeManagementSystem/Controllers/CustomerController.cs
+++ FridgeManagementSystem/Controllers/CustomerController.cs
@@ -1,5 +1,7 @@
-﻿using FridgeManagementSystem.Data;
+﻿using FridgeManagementSystem.Areas.Identity.Data;
+using FridgeManagementSystem.Data;
 using FridgeManagementSystem.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,55 +9,89 @@ namespace FridgeManagementSystem.Controllers
 {
     public class CustomerController : Controller
     {
-        private readonly FridgeManagementSystemContext _db;
-        public CustomerController(FridgeManagementSystemContext db) { _db = db; }
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<ApplicationUser> userManager;
 
-        public async Task<IActionResult> Index() => View(await _db.Customers.Include(c => c.Allocations).ToListAsync());
-        //public IActionResult Index()
+        public CustomerController(RoleManager<IdentityRole> roleManager,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+
+        }
+
+        public async Task<IActionResult> ListCustomers()
+        {
+
+            //var user = userManager.Users.ToList();
+            var customer = await userManager.GetUsersInRoleAsync("Customer");
+           
+            return View(customer);
+        }
+        //public async Task<IActionResult> ListUsers()
         //{
-        //    return View();
+        //    var users = userManager.Users.ToList();
+        //    var userRoles = new List<object>();
+        //    foreach (var user in users)
+        //    {
+        //        var roles = await userManager.GetRolesAsync(user);
+        //        userRoles.Add(new
+        //        {
+        //            user.UserName,
+        //            user.Email,
+        //            Roles = roles
+        //        });
+        //    }
+
+        //    return Json(userRoles);
+        //public CustomerController(FridgeManagementSystemContext db) { _db = db; }
+
+        //public async Task<IActionResult> Index() => View(await _db.Customers.Include(c => c.Allocations).ToListAsync());
+        ////public IActionResult Index()
+        ////{
+        ////    return View();
+        ////}
+        //public IActionResult Create() => View();
+
+        //[HttpPost]
+        //public async Task<IActionResult> Create(Customer customer)
+        //{
+        //    if (!ModelState.IsValid) return View(customer);
+        //    _db.Add(customer);
+        //    await _db.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
         //}
-        public IActionResult Create() => View();
+        //public async Task<IActionResult> Edit(int id)
+        //{
+        //    var customer = await _db.Customers.FindAsync(id);
+        //    if (customer == null) return NotFound();
+        //    return View(customer);
+        //}
 
-        [HttpPost]
-        public async Task<IActionResult> Create(Customer customer)
-        {
-            if (!ModelState.IsValid) return View(customer);
-            _db.Add(customer);
-            await _db.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-        public async Task<IActionResult> Edit(int id)
-        {
-            var customer = await _db.Customers.FindAsync(id);
-            if (customer == null) return NotFound();
-            return View(customer);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Edit(Customer customer)
-        {
-            if (!ModelState.IsValid) return View(customer);
-            _db.Update(customer);
-            await _db.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-        public async Task<IActionResult> Delete(int id)
-        {
-            var customer = await _db.Customers.FindAsync(id);
-            if (customer == null) return NotFound();
-            _db.Customers.Remove(customer);
-            await _db.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-        public async Task<IActionResult> Details(int id)
-        {
-            var customer = await _db.Customers
-                .Include(c => c.Allocations)
-                .ThenInclude(a => a.Fridge)
-                .FirstOrDefaultAsync(c => c.CustomerId == id);
-            if (customer == null) return NotFound();
-            return View(customer);
-        }
+        //[HttpPost]
+        //public async Task<IActionResult> Edit(Customer customer)
+        //{
+        //    if (!ModelState.IsValid) return View(customer);
+        //    _db.Update(customer);
+        //    await _db.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+        //public async Task<IActionResult> Delete(int id)
+        //{
+        //    var customer = await _db.Customers.FindAsync(id);
+        //    if (customer == null) return NotFound();
+        //    _db.Customers.Remove(customer);
+        //    await _db.SaveChangesAsync();
+        //    return RedirectToAction(nameof(Index));
+        //}
+        //public async Task<IActionResult> Details(int id)
+        //{
+        //    var customer = await _db.Customers
+        //        .Include(c => c.Allocations)
+        //        .ThenInclude(a => a.Fridge)
+        //        .FirstOrDefaultAsync(c => c.CustomerId == id);
+        //    if (customer == null) return NotFound();
+        //    return View(customer);
+        //}
     }
 }
