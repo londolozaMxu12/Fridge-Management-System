396c459f5c97ca32e94d3f7202de82771744c277
Londoloza Mxunyelwa <s225302985@mandela.ac.za>
2025-10-09 00:07:12 +0200

    I added repair controller

---
+++ FridgeManagementSystem/Controllers/RepairScheduleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FridgeManagementSystem.Controllers
+{
+    public class RepairScheduleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
